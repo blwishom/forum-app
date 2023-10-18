0858fc5661662a4932dd39356a6e2236a4fd5f9a
Blair Wishom <blwishom@gmail.com>
2023-10-18 07:09:01 -0700

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React, {useState} from "react"; 
+import React, {useState} from "react";
 import {Routes,Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
-import Login from "./components/login/Login"; 
-import Signup from "./components/signup/Signup"; 
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
 import CreatePost from "./components/create-post/CreatePost";
 import AuthContextProvider from "./service/AuthContextProvider";
 
 function App() {
-  const [isLoggedIn] = useState(false); 
-  
+  const [isLoggedIn] = useState(false);
+
     return (
-      
         <div className="App">
           <AuthContextProvider value={( isLoggedIn )}>
           <Navbar/>
             <Routes>
               <Route path="/" exact element={<Dashboard/>}/>
               <Route path="/login" exact element={<Login/>}/>
-              <Route path="/signup" exact element={<Signup/>}/>         
-              <Route path="/create-post" exact element={<CreatePost/>}/>     
+              <Route path="/signup" exact element={<Signup/>}/>
+              <Route path="/create-post" exact element={<CreatePost/>}/>
             </Routes>
           </AuthContextProvider>
-        </div>)
+        </div>
+        )
 }
-export default App; 
+export default App;
