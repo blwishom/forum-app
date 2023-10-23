ac46c846b769946821f57a8b902ded42ca2911fb
nedned1108@github.com <rpmshockwave@gmail.com>
2023-10-23 10:40:13 -0400

    updated delete post

--- client/src/components/user-post/UserPost.js
+++ client/src/components/user-post/UserPost.js
@@ -28,9 +28,30 @@ function UserPost() {
     navigate(`/post/${post_id}`)
   }
 
+  const deletePost = async (e, post_id) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:8800/api/post/${post_id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ post_id: post_id }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const user_posts = data.data.filter(post => post.author.username === username);
+        setPosts(user_posts);
+      }
+    } catch (error) {
+      setErrorMessage("An error occurred. Please try again later.");
+    }
+  }
+
   useEffect(() => {
     getData();
-  }, [])
+  }, [posts])
 
   return (
     <div>
@@ -40,14 +61,16 @@ function UserPost() {
       </div>
       {!posts && <div className='post-header'>No posts found.</div>}
       {posts && posts.map((post) => (
-        <div onClick={(e) => toPost(e, post._id)} className="post-container">
-          <div className="post-header">
-            <div className="post-title">{post.title}</div>
+        <>
+          <div onClick={(e) => toPost(e, post._id)} className="post-container">
+            <div className="post-header">
+              <div className="post-title">{post.title}</div>
+            </div>
+            <div className="post-content">{post.content}</div>
+            <div className="post-author">Posted by {post.author.username}</div>
           </div>
-          <div className="post-content">{post.content}</div>
-          <div className="post-author">Posted by {post.author.username}</div>
-          <button>Delete Post</button>
-        </div>
+          <button onClick={(e) => deletePost(e, post._id)}>Delete Post</button>
+        </>
       ))
       }
     </div>
